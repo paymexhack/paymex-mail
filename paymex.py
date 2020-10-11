ef76b3226aad22fdc1ec4c4dfbfec00b8a96b823
paymexhack <71406765+paymexhack@users.noreply.github.com>
2020-10-11 10:08:07 +0200

    Create paymex.py
    Guess the accounts

---
+++ paymex.py
@@ -0,0 +1,92 @@
+import os
+
+os.system("clear")
+
+A='\033[1;36m'   
+
+print(A)                                            
+
+print("                    g@@")
+
+print("A.       PAYMEX    d@V@")
+
+print("i@b               iA` @         HACK        .A")
+
+print("i@V@s            dA`  @                   ,@@i")
+
+print("i@ '*@Ws______mW@f    @@                  ]@@@")
+
+print(" @]  '^********f`     @@@                g@!l@")
+
+print(" @b           .  ,g   @@@W             ,W@[ l@")
+
+print(" ]@   Mmmmmf  ~**f^   @@^@@Wm_.      _g@@`  l@")
+
+print("  M[    ^^      ^^    @@  ^*@@@@@@@@@@@f`   l@")
+
+print("  ]@i  '[]`  . '[]`   @@      ~~~~~~~`      l@")
+
+print("  '@W       |[        @@               _.   l@")
+
+print("   Y@i      'Ns    .  @@   _m@@m,    g*~VWi l@")
+
+print("    M@. ,.       g@` ]@@  gP    V,  '  m- ` l@")
+
+print("    '@W  Vmmmmmm@f   @@@. ~  m-    g   '    l@")
+
+print("     '@b  'V***f    g@A@W    '     @        l@")
+
+print("      '*W_         g@A`M@b       g M_.      l@")
+
+print("        'VMms___gW@@f   @@.      '- ~`      W@")
+
+print("           ~*****f~`     @@.     ,_m@Ws__   @@")
+
+print("                         '@@.   gA~`   ~~* ]@P")
+
+print("                           M@s.'`         g@A")
+
+print("                            V@@Ws.    __m@@A")
+
+print("                              ~*M@@@@@@@@*`")
+
+A='\033[1;31m'
+
+B='\033[1;32m'
+
+C='\033[1;33m'
+
+print(A) 
+
+name=input("Enter name: ") 
+
+print (B) 
+
+email=input (" Enter email: ") 
+
+if email=="@gmail.com" or "@yahoo.com" or "@hotmail.com":
+
+    print("DONE...!")
+
+else:
+
+    print(" ERROR.!!!!!!")
+
+import random
+
+pa=random.randint(1,9)
+
+number=pa  
+
+while number<500:
+
+      number=number+pa   
+
+      print(C)
+
+      print('[+]',name+str(number)+email)
+
+      
+
+print("  ")
+
